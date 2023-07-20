edf8aa89a0ef7068b37da1df5b799da54448242f
alo40 <alejandronieto40@gmail.com>
2023-07-20 22:39:51 +0200

    add rust_tets.rs

---
+++ rust_test.rs
@@ -0,0 +1,10 @@
+// Mi primer rust programa
+
+
+// This is the main function.
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+    println!("Hello World!");
+}
