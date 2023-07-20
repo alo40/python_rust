f3a4527024abccd520f7176db052dd42534e1f89
alo40 <alejandronieto40@gmail.com>
2023-07-20 22:57:28 +0200

    add mystery_functions.rs

---
+++ mystery_functions.rs
@@ -0,0 +1,13 @@
+// Mystery Function example
+
+fn mystery_function(input: &str) -> i32 {
+    let mysteries = "aeiouAEIOU";
+    input.chars().filter(|c| mysteries.contains(*c)).count() as i32
+}
+
+fn main(){
+    let input_string = "Hello, world!";
+    let mystery_count = mystery_function(&input_string);
+    println!("The number of mysteries in '{}' is {}", input_string, mystery_count);
+
+}
